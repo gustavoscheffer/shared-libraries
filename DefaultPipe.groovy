c1d4f12463d119fc006004ed232b1e73f19723ea
Gustavo Scheffer <gustavo.scheffer@outlook.com>
2021-09-05 23:51:47 -0300

    Testing my new methods

--- DefaultPipe.groovy
+++ DefaultPipe.groovy
@@ -1,36 +1,37 @@
 @Library('shared-libraries') _
-import com.mycorp.somelib.Helper
+evenOrOdd(currentBuild.getNumber())
 
-pipeline {
-    agent any
-    stages {
-        stage('Build') {
-            steps {
-                helper = new Helper('Jason Bourne', 34)
-                helper.prepare()
-                echo 'Build'
 
-            }
-        }
-        stage('Unit Test') {
-            steps {
-                echo 'Unit Test'
-            }
-        }
-        stage('Deploy Stage') {
-            steps {
-                echo 'Deploy Stage'
-            }
-        }
-        stage('UAT') {
-            steps {
-                echo 'User Acceptance Test'
-            }
-        }
-        stage('Deploy Production') {
-            steps {
-                echo 'Deploy Production'
-            }
-        }
-    }
-}
+// pipeline {
+//     agent any
+//     stages {
+//         stage('Build') {
+//             steps {
+//                 helper = new Helper('Jason Bourne', 34)
+//                 helper.prepare()
+//                 echo 'Build'
+
+//             }
+//         }
+//         stage('Unit Test') {
+//             steps {
+//                 echo 'Unit Test'
+//             }
+//         }
+//         stage('Deploy Stage') {
+//             steps {
+//                 echo 'Deploy Stage'
+//             }
+//         }
+//         stage('UAT') {
+//             steps {
+//                 echo 'User Acceptance Test'
+//             }
+//         }
+//         stage('Deploy Production') {
+//             steps {
+//                 echo 'Deploy Production'
+//             }
+//         }
+//     }
+// }
